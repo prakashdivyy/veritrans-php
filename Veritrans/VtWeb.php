e997a39a75e22127fdd637b14f79bef027989fab
prakashdivyy <prakashdivyy@gmail.com>
2017-02-27 19:49:00 +0700

    Fixing enable payment

--- Veritrans/VtWeb.php
+++ Veritrans/VtWeb.php
@@ -30,7 +30,7 @@ class Veritrans_VtWeb {
     $payloads = array(
       'payment_type' => 'vtweb',
       'vtweb' => array(
-        // 'enabled_payments' => array('credit_card'),
+        'enabled_payments' => $params['enable_payments'],
         'credit_card_3d_secure' => Veritrans_Config::$is3ds
       )
     );
